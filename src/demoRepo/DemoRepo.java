742df84baea5305b56eb8ba5df0a1a62ea3dfa0a
Jesse Gronewold <jessegronewold05@gmail.com>
2024-11-05 13:33:10 -0600

    first commit

---
+++ src/demoRepo/DemoRepo.java
@@ -0,0 +1,14 @@
+package demoRepo;
+
+public class DemoRepo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+boolean amIStrugglingToLearnCode = true;
+
+System.out.println(amIStrugglingToLearnCode);
+
+	} 
+
+}
